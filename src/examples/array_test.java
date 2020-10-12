2632c9c5bc6cf4ffb05872dc1b54dab2967343bf
Evgeniy Pasukov <pasukoff@mail.ru>
2020-10-12 22:52:05 +0300

    update

--- src/examples/array_test.java
+++ src/examples/array_test.java
@@ -5,7 +5,10 @@ package examples;
 public class cs_test {
     public static void main(String[] args) {
         int[] name = new int[] {21, 22, 44};
-        System.out.println(name);
+        for (int i =0; i<name.length; i++){
+            System.out.println(name[i]);
+
+        }
 
         }
 
---
+++ src/examples/files_test.java
@@ -0,0 +1,29 @@
+package examples;
+import java.io.*;
+
+public class java_test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = null;
+
+        try {
+            File file = new File("111.txt");
+
+            if (!file.exists())
+                file.createNewFile();
+
+            PrintWriter pw = new PrintWriter(file);
+            pw.println("Ok");
+            pw.close();
+        br = new BufferedReader(new FileReader("111.txt"));
+        String line;
+        while ((line=br.readLine()) != null){
+            System.out.println(line);
+        }
+        } catch (IOException e){
+            System.out.println("error");
+        }finally {
+           br.close();
+        }
+
+    }
+}
